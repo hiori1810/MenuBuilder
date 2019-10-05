6db552dcc75f9b6b1c3fd21ed7bf39e36822ce7e
hiori1810 <hiori181098@gmail.com>
2019-10-05 09:54:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/menubuilder/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.menubuilder;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.menu_example,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mnxemds:
+                Toast.makeText(this,"Xem danh sach",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.mnxemdssv:
+                Toast.makeText(this,"Xem danh sach sinh vien",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.mnsuads:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
